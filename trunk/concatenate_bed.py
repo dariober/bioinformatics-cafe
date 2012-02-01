76f8db06ce07e290f78062a6d554285ea8e83c60
dario.beraldi@gmail.com <dario.beraldi@gmail.com@7f1e2009-dfb0-8c77-38af-1b75acbe61a4>
2012-02-01 17:38:44 +0000

    concatenate_bed.py add to library

---
+++ trunk/concatenate_bed.py
@@ -0,0 +1,112 @@
+#!/usr/local/bin/python
+
+
+import os
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description= """
+
+DESCRIPTION:
+
+    Concatenate several bed files in a single one and adds a column identifying
+    for each line the source file.
+    An error is thrown if one tries to concatenate a file to itself.
+    
+    This program concatenate any file, not just beds and no checking is done
+    about whether the files have bed formatting.
+    
+EXAMPLES:
+    
+    Concatenate all bed files in current dir and strip the .bed extension
+    
+    ls *.bed | concatenate_bed.py -i - -o concoutput.bed -s .bed
+
+    Output (last column is the file name):
+--------------------------------------------------------------------------------
+chr1    3521705 3521924 CpG:_27 2       52      219     0.2374429       bham359.coverage
+chr1    3660700 3661155 CpG:_34 11      302     455     0.6637363       bham359.coverage
+chr1    3661735 3662237 CpG:_45 10      311     502     0.6195219       bham359.coverage
+...
+--------------------------------------------------------------------------------
+
+TODO:
+      
+""", formatter_class= argparse.RawTextHelpFormatter)
+
+parser.add_argument('-i', '--input',
+                    nargs='+',
+                    type= str,
+                    required= True,
+                    help="""Input bed files(s) to be concatebated. Use -i - to
+read the list of files from stdin (e.g. ls *.bed | concatenate_bed.py -i -)
+                    """)
+
+parser.add_argument('-o', '--output',
+                    type= str,
+                    required= True,
+                    help="""Name of output file.
+                    """)
+
+parser.add_argument('-s', '--strip',
+                    type= str,
+                    required= False,
+                    help="""String to strip from the input file name. The
+resulting name will be used as identifier of the source file. Defualt is not
+to strip anything.
+                """)
+
+parser.add_argument('-d', '--dir',
+                    action= 'store_true',
+                    required= False,
+                    help="""With this flag the file id in output will include
+the directory path. Defualt is to strip the path. 
+                """)
+
+parser.add_argument('--skip',
+                    type= int,
+                    default= 0,
+                    required= False,
+                    help="""Skip this many lines from each input bed before
+writing out (e.g. use --skip 1 to skip the header). 
+                """)
+
+#parser.add_argument('-H', '--header',
+#                    type= str,
+#                    required= False,
+#                    help="""Optional string to use as header.
+#                    """)
+
+args = parser.parse_args()
+
+if args.input == ['-']:
+    args.input= sys.stdin.readlines()
+    args.input= [x.strip() for x in args.input]
+if args.output in args.input:
+    sys.exit('%s error: Output file %s also present in input list' %(os.path.basename(__file__), args.output))
+
+
+#if args.header:
+#    print(args.header)
+
+fout= open(args.output, 'w')
+
+for f in args.input:
+    print('Concatenating: %s' %(f))
+    n= 0
+    file_id= f
+    if args.dir is False:
+        file_id= os.path.split(f)[1]
+    if args.strip:
+        file_id= file_id.replace(args.strip, '')
+    fin= open(f)
+        
+    for line in fin:
+        if n < args.skip:
+            n += 1
+            continue
+        line= line.rstrip('\n\r')
+        line= line + '\t' + file_id
+        fout.write(line + '\n')
+    fin.close()
+fout.close()
