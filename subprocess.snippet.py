7683864318acb18490ef25f4efc1b2c7e0f9b12a
dario.beraldi@gmail.com <dario.beraldi@gmail.com@7f1e2009-dfb0-8c77-38af-1b75acbe61a4>
2012-03-09 15:58:52 +0000

    Snippet for subprocess

---
+++ subprocess.snippet.py
@@ -0,0 +1,35 @@
+#!/home/berald01/.local/bin/python
+
+"""
+Snippet to launch a number of shell subprocess using a limited number
+if cores.
+"""
+
+import subprocess
+import os
+import time
+
+NCORES= 6
+
+bams= [x for x in sorted(os.listdir(os.getcwd())) if x.endswith('.bam')]
+
+procs= []
+running_procs= 0
+n= 0
+for bam in bams:
+    n += 1
+    if n == 1:
+        cmd= 'bamqc.py -i %s --noheader --nograph > bamqc.tsv' %(bam)
+    else:
+        cmd= 'bamqc.py -i %s --noheader --nograph >> bamqc.tsv' %(bam)
+    print('%s. %s' %(n, cmd))
+    p= subprocess.Popen(cmd, shell= True)
+    procs.append(p)
+    running_procs += 1
+    while running_procs >= NCORES:
+        time.sleep(5)
+        running_procs= 0
+        for x in procs:
+            xv= x.poll()
+            if x.returncode is None:
+                running_procs += 1
