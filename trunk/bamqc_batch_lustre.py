ba3cfc6f016360b8f5ea9016dbb74357faf6260f
dario.beraldi@gmail.com <dario.beraldi@gmail.com@7f1e2009-dfb0-8c77-38af-1b75acbe61a4>
2012-03-14 12:25:57 +0000

    add batch bamqc

---
+++ trunk/bamqc_batch_lustre.py
@@ -0,0 +1,27 @@
+#!/home/berald01/.local/bin/python
+
+"""
+Execute bamqc.py on all the bam files under BAMQCDIR (searched recursivley)
+Output files sent to BAMDIR.
+
+bamqc.py is executed on lustre using bsub.
+"""
+
+import subprocess
+import os
+
+BAMQCDIR= '/home/berald01/bamqc' ## dir where output files will be
+BAMDIR= '/lustre/sblab/berald01' ## Top dir where to serach for bams
+
+p= subprocess.Popen('find %s' %(BAMDIR), shell= True, stdout=subprocess.PIPE)
+allfiles= p.stdout.read().strip()
+allfiles= allfiles.split('\n')
+bamfiles= [x for x in allfiles if x.endswith('.bam')]
+if not os.path.exists(BAMQCDIR):
+    os.makedirs(BAMQCDIR)
+
+for bam in bamfiles:
+    bamqc_out= os.path.join(BAMQCDIR, os.path.splitext(os.path.split(bam)[1])[0] + '.bamqc.tsv')
+    cmd= 'bsub -R "rusage[MEM=1024]" "bamqc.py --nograph --noheader -i %s -o %s"' %(bam, bamqc_out)
+    print(cmd)
+    p= subprocess.Popen(cmd, shell= True)
