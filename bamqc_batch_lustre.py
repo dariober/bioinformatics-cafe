fe57114cd5c73172653aea329039e13a3808075c
dario.beraldi@gmail.com <dario.beraldi@gmail.com@7f1e2009-dfb0-8c77-38af-1b75acbe61a4>
2012-03-14 14:12:25 +0000

    add batch bamqc

--- bamqc_batch_lustre.py
+++ bamqc_batch_lustre.py
@@ -12,6 +12,7 @@ import os
 
 BAMQCDIR= '/lustre/sblab/berald01/bamqc' ## dir where output files will be
 BAMDIR= '/lustre/sblab/berald01' ## Top dir where to serach for bams
+LOCALHOST= '143.65.172.155:/tmp/' ## Where the output of bamqc will be scp'd
 
 p= subprocess.Popen('find %s' %(BAMDIR), shell= True, stdout=subprocess.PIPE)
 allfiles= p.stdout.read().strip()
@@ -23,6 +24,14 @@ if not os.path.exists(BAMQCDIR):
 for bam in bamfiles:
     bamqc_out= os.path.join(BAMQCDIR, os.path.splitext(os.path.split(bam)[1])[0] + '.bamqc.tsv')
     bamqc_log= os.path.join(BAMQCDIR, os.path.splitext(os.path.split(bam)[1])[0] + '.bamqc.log')
-    cmd= 'bsub -o %s "bamqc.py --nograph --noheader -i %s -o %s"' %(bamqc_log, bam, bamqc_out) # -R "rusage[MEM=1024]"
+    jobname= 'jobbamqc_' + bam
+    # cmd= 'bsub -R "rusage[mem=1024]" -J %s -o %s "bamqc.py --nograph --noheader -i %s -o %s"' %(jobmame, bamqc_log, bam, bamqc_out)
+    cmd= 'bsub -R "rusage[mem=1024]" -J %s -o %s "ls"'
     print(cmd)
     p= subprocess.Popen(cmd, shell= True)
+
+cmd= """bsub -R "rusage[mem=1024]" -w jobbamqc_* "
+cat %(bamqcdir)s/*.bamqc.tsv > %(cat_bamqc)s;
+scp $(cat_bamqc)s %(localdir)s"
+""" %{'bamqcdir':BAMQCDIR.rstrip('/'), 'cat_bamqc': os.path.join(BAMQCDIR, 'bamqc.tsv'), 'localdir':LOCALHOST}
+p= subprocess.Popen(cmd, shell= True)
