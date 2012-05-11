15eb357bd7668a5340b6f9c4d32a28d04e2dc40a
dario.beraldi@gmail.com <dario.beraldi@gmail.com@7f1e2009-dfb0-8c77-38af-1b75acbe61a4>
2012-05-11 07:53:56 +0000

    Add args top format_table

--- trunk/format_table.py
+++ trunk/format_table.py
@@ -17,10 +17,14 @@ parser = argparse.ArgumentParser(description= """
     hmc.enriched.bed  319880500  fc.enriched.bed 314660500  167726500  52.43  152154000  47.57
     
     USAGE
-        format_table.py <table-file> <min-space>
+        format_table.py <table-file>
     <table-file>: File to format. Use - to read from stdin (e.g. less myfile | format_table.py -)
-    <min-space>:  Minimum number of spaces to separate the columns. Default: 4
-
+    Minimum number of spaces to separate the columns. Default: 4
+    
+    Read ten lines starting from line 3:    
+        format_table.py -m 10 -N 3 <table-file>
+    
+        
     TODO:
         - Read CSV files: Now a column is split at every space (tab or space, one or more times).
         - Allow custom separator (pass to line.split())
@@ -49,6 +53,21 @@ parser.add_argument('-s', '--sep',
     Typing '\t' will be interpreted literally.
                     """)
 
+parser.add_argument('-m', '--maxlines',
+                    type= int,
+                    default= 0,
+                    help="""Maximum number of lines to read.
+                    An integer <= 0 will read the entire file (in memory)
+    Default is 0 (read entire file).
+                    """)
+
+parser.add_argument('-N', '--nstart',
+                    type= int,
+                    default= 1,
+                    help="""Start reading the file from this line number. 1-based
+    Meaning that --nstart 1 will read from the beginning (default)
+                    """)
+
 args = parser.parse_args()
 
 # -----------------------------------------------------------------------------
@@ -59,22 +78,35 @@ else:
 
 spacer= ' ' * args.nsep 
 # -----------------------------------------------------------------------------
-print(args.sep)
+
+## Go to the requested line number
+n= 1
+while n < args.nstart:
+    fh.readline()
+    n += 1
+
+## Prepare list of lists from the table-file: 
 table= []
 maxcols= 0
+n= 0
 for line in fh:
+    n += 1
     line= line.strip().split(args.sep)
     if len(line) > maxcols:
         maxcols= len(line)
     table.append(line)
+    if (args.maxlines > 0) and (n >= args.maxlines):
+        break
 
 ## Get max column size for each column:
-colwidths= [0] * maxcols 
+colwidths= [0] * maxcols
 for i in range(0, maxcols):
     for line in table:
         cell= line[i]
         if len(cell) > colwidths[i]:
             colwidths[i]= len(cell)
+
+## Print put the with the correct spacing
 for line in table:
     for i in range(0, len(line)):
         colOffset= colwidths[i]
