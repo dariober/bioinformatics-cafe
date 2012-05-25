3d519b5fbb6d980da937e43414603a84546be2f7
dario.beraldi@gmail.com <dario.beraldi@gmail.com@7f1e2009-dfb0-8c77-38af-1b75acbe61a4>
2012-05-25 17:57:54 +0000

    pairwisebed allows beds with header

--- trunk/pairwise_bed_overlap.py
+++ trunk/pairwise_bed_overlap.py
@@ -3,10 +3,15 @@
 import sys
 import shlex
 import subprocess
+import tempfile
+import os
+import shutil
 
 if len(sys.argv) < 2:
     sys.exit("""
-Given a list of bed files, calculates the pairwise overlap.
+Given a list of bed files, calculates the pairwise overlap. If the first line of
+each file does not start with <str>\t<int>\t<int> it is assumed it is header and it will be
+skipped.
 
 Columns:
     1. File A
@@ -35,6 +40,44 @@ It is assumed that sumbed.py returns a string tab separated as: <filenaname> <sp
 
 """)
 
+def skipBedHeader(inbed, truebed= None):
+    """ Check if the first line of a bed file is the header. If so, output a new file
+    without the first line. Checking is simply done by checking if columns 2nd
+    and 3rd are int.
+    
+    inbed: File name of input bed
+    Output: Output filename. Default is None which sends the new file to temp dir
+    
+    Returns:
+    None if the input bed file has no header. If instead the newfile has been
+    produced returns the full path ti the file
+    """
+    bname= os.path.split(inbed)[1]
+    if truebed is None:
+        tdir= tempfile.mkdtemp(prefix= 'pwise_')
+        truebed= os.path.join(tdir, bname)
+
+    fin= open(inbed)
+    headerline= fin.readline().strip().split('\t')
+    try:
+        int(headerline[1])
+        try:
+            int(headerline[2])
+            header= False
+            fin.close()
+            return(None) ## Second and Third cols of first line are int. Exit here
+        except ValueError:
+            header= True
+    except ValueError:
+        header= True
+        
+    fout= open(truebed, 'w')
+    for line in fin:
+        fout.write(line)
+    fin.close()
+    fout.close()
+    return(truebed)
+
 class pairwise:
     def __init__(self):
         self.filea= ''
@@ -44,12 +87,21 @@ class pairwise:
         self.fileb_span= 0
         self.filea_count= 0
         self.diff= 0
-        
-        
     
 ## files= shlex.split(sys.argv[1])
 files= sys.argv[1:]
 files= sorted(set(files))
+
+## Remove headers as required
+tempfiles= []
+for i in range(0, len(files)):
+    out= skipBedHeader(files[i])
+    if out is None:
+        pass
+    else:
+        tempfiles.append(out)
+        files[i]= out
+
 ## Number of bases spanned by each bed file
 filespans= {}
 for f in files:
@@ -65,19 +117,12 @@ for fa in files:
     for fb in files:
         if fa == fb:
             continue
-            #diff= 0
-            #inters= filespans[fa]
         else:
             cmd= 'subtractBed -a %s -b %s | sumbed.py -' %(fa, fb)
             p= subprocess.Popen(cmd, shell= True, stdout= subprocess.PIPE)
             p.wait()
             sumout= p.stdout.read().rstrip('\n\r')
             diff= int(sumout.split('\t')[1])
-#            cmd= 'intersectBed -a %s -b %s | sumbed.py -' %(fa, fb)
-#            p= subprocess.Popen(cmd, shell= True, stdout= subprocess.PIPE)
-#            p.wait()
-#            sumout= p.stdout.read().rstrip('\n\r')
-            
         pair= pairwise()
         pair.filea= fa
         pair.fileb= fb
@@ -92,4 +137,8 @@ for fa in files:
         line= ([pair.filea, pair.filea_span, pair.filea_count, pair.fileb, pair.fileb_span, pair.fileb_count, inters, perc_cov, diff, perc_unc])
         line= [str(x) for x in line]
         print('\t'.join(line))
+
+## Clean up temp files if any:
+for f in tempfiles:
+    shutil.rmtree(os.path.split(f)[0])
 sys.exit()
