294dd465e2eea54a20d01c573a40d9b4021fcaf7
dario.beraldi@gmail.com <dario.beraldi@gmail.com@7f1e2009-dfb0-8c77-38af-1b75acbe61a4>
2012-09-20 09:30:47 +0000

    Script to make matrix from htseq-count

---
+++ trunk/htseq_matrix.py
@@ -0,0 +1,105 @@
+#!/home/berald01/.local/bin/python
+
+import os
+import sys
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description= """
+DESCRIPTION
+    Produces a matrix of genes (rows) and libraries (columns) from the output of
+    htseq-count. Output will be suitable for edgeR/DESeq.
+    Stdout will print the rows from "no_feature" to the end.
+
+EXAMPLE
+    ## Produce matrix of all the files ending in .htseq, remove .htseq from file name
+    find . -name '*.htseq' | htseq_matrix.py - -s '\.htseq$' htseq.matrix
+TODO:
+
+""", formatter_class= argparse.RawTextHelpFormatter)
+
+parser.add_argument('htseqfiles',
+                   nargs= '+',
+                   help='''One or more output files from htseq-count to be converted
+to matrix. Output files should have the same features in the same order. Use - to
+read the list of files from stdin.
+                   ''')
+
+parser.add_argument('--outmatrix', '-o',
+                   required= True,
+                   type= str,
+                   help='''Output file where the matrix will be written to.
+                   ''')
+
+parser.add_argument('--strip', '-s',
+                   required= False,
+                   type= str,
+                   default= None,
+                   help='''Remove from each file name this regular expression.
+The replacement is applied to the file name only unless --keepdir is active. With
+--keepdir the replacement is applied to the whole path as passed to the list of files.
+                   ''')
+
+parser.add_argument('--keepdir', '-d',
+                   required= False,
+                   action= 'store_true',
+                   help='''Do not remove directory path from file names (by default
+it is removed).
+                   ''')
+
+args = parser.parse_args()
+
+# ------------------------------------------------------------------------------
+
+if args.htseqfiles == ['-']:
+    args.htseqfiles= sys.stdin.readlines()
+    args.htseqfiles= [x.strip() for x in args.htseqfiles]
+    
+if args.keepdir is False:
+    header= [os.path.split(x)[1] for x in args.htseqfiles] ## Remove path
+else:
+    header= [x for x in args.htseqfiles]
+if args.strip is not None:
+    header= [re.sub(args.strip, '', x) for x in header]
+header= '\t'.join(['feature_id'] + header)
+
+outf= open(args.outmatrix, 'w')
+outf.write(header + '\n')
+print(header)
+
+is_feature= True ## Mark to say whether the current line is feature to be inlcuded in the matrix
+                 ## Will turn to False once the row 'no_feature' is found
+
+infiles= []
+for lib in args.htseqfiles:
+    infiles.append(open(lib))
+while True:
+    for i in range(0, len(infiles)):
+        if i == 0:
+            fline= infiles[i].readline()
+            fline= fline.strip().split('\t')
+            feature_id= fline[0]
+            if feature_id == 'no_feature':
+                is_feature= False
+            if fline == ['']:
+                break
+            line= fline
+        else:
+            fline= infiles[i].readline()
+            fline= fline.strip().split('\t')
+            if fline == ['']:
+                break
+            next_feature= fline[0]
+            if next_feature != feature_id:
+                sys.exit('Input files do not seem to have the same features and/or they are not in the same order: %s, %s' %(feature_id, next_feature))
+            line.append(fline[1])
+    if fline == ['']:
+        break
+    if is_feature:
+        outf.write('\t'.join(line) + '\n')
+    else:
+        print('\t'.join(line))
+for f in infiles:
+    f.close()    
+outf.close()
+sys.exit()
