490b19a8b5ee9038a9300d951291e75290bbe9ef
dario.beraldi@gmail.com <dario.beraldi@gmail.com@7f1e2009-dfb0-8c77-38af-1b75acbe61a4>
2012-11-06 09:19:51 +0000

    fix bug and docs in fastqc_to_pgtable.py

--- trunk/sumbed.py
+++ trunk/sumbed.py
@@ -3,9 +3,10 @@
 import sys
 import subprocess
 import os
+import pybedtools
 
 if len(sys.argv) != 2 or sys.argv[1] == '-h':
-    sys.exit("""
+    sys.exit('''
 
 Return the sum of the regions spanned in a bed file after having merged the
 overlapping features.
@@ -34,19 +35,17 @@ sumbed.py >>> (10-1) + (25-20)= 14
 
 Note: Requires mergeBed and input file must fit in memory
 
-""")
+''')
 # ------------------------------------------------------------------------------
 def sumbed(bedlist):
     """ Sum the features in a bed file to compute the overall span
-    bedlist: list where each element is a line of the bed file (string).
-             Typically returned by bedlist= open('mybed.bed').readlines()
+    bedlist: list of list where each inner list is a line of a bed file (string).
     
     Returns a list of length 2: ['span bp', 'n lines']
     """
     span= 0
     n= 0
     for line in bedlist:
-        line= line.rstrip('\n\r').split('\t')
         span += int(line[2]) - int(line[1])
         n += 1
     return([span, n])
@@ -54,7 +53,7 @@ def sumbed(bedlist):
 fin= sys.argv[1]
 
 if fin == '-':
-    " Read bed ffrom stdout "
+    " Read bed from stdout "
     fouttmp= 'sumbed.tmp.bed'
     fh= open(fouttmp, 'w')
     filein= sys.stdin
@@ -66,23 +65,44 @@ if fin == '-':
             break
     fin= fouttmp
     
-ftmp= sys.argv[1] + '.sumbed.bed'  ## Merged input bed
-cmd= 'mergeBed -i %s > %s' %(fin, ftmp)
-p= subprocess.Popen(cmd, shell= True)
-p.wait()
-bed= open(ftmp).readlines()
+## ftmp= sys.argv[1] + '.sumbed.bed'  ## Merged input bed
+## cmd= 'mergeBed -i %s > %s' %(fin, ftmp)
+## p= subprocess.Popen(cmd, shell= True)
+## p.wait()
+## bed= open(ftmp).readlines()
+
+# ----------------------
+## Stats for merged file
+## ---------------------
+bedin= pybedtools.BedTool(fin) ## Read-in input bed file
+bedmerged= bedin.merge()## .merge() ## Merge it
+
+bed= []
+for line in bedmerged:
+    bedline= []
+    for x in line:
+        bedline.append(x)
+    bed.append(bedline)
+
 sumsmerged= sumbed(bed)
 
-bed= open(fin).readlines()
-sums= sumbed(bed)
+## Stats for original file
+bedOri= open(fin).readlines()
+bedList= []
+for line in bedOri:
+    line= line.rstrip('\n\r').split('\t')
+    bedList.append(line)
+
+sums= sumbed(bedList)
 
 print('\t'.join([fin, str(sumsmerged[0]), str(sumsmerged[1]), str(sums[0]), str(sums[1])]))
+
 #print('Size spanned (bp):                %s' %(sumbed))
 #print('Number of features after merging: %s' %(n))
 
-os.remove(ftmp)
-try:
-    os.remove(fouttmp)
-except:
-    pass
+#os.remove(ftmp)
+#try:
+#    os.remove(fouttmp)
+#except:
+#    pass
 sys.exit()
