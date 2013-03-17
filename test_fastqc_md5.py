d4f58b2705f45afa6703575b5581730c8ac0c401
dario.beraldi@gmail.com <dario.beraldi@gmail.com@7f1e2009-dfb0-8c77-38af-1b75acbe61a4>
2013-03-17 08:06:50 +0000

    test for fastqc_md5

---
+++ test_fastqc_md5.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env py.test
+
+import fastqc_md5
+import shlex
+import subprocess
+import tempfile
+import os
+import shutil
+
+def test_getFastqcOutdir():
+    fastqccmd= shlex.split('-t 8 --noextract -o /test/dir')    
+    outd= fastqc_md5.getFastqcOutdir(fastqccmd)
+    assert outd == '/test/dir'
+
+def test_md5sum():
+    tmpf= tempfile.NamedTemporaryFile()
+    md5= fastqc_md5.md5sum(tmpf.name)
+    assert md5 == 'd41d8cd98f00b204e9800998ecf8427e' ## This is md5 of NULL. possibly make better test
+
+def test_add_md5_fastqc():
+    tmpf= tempfile.NamedTemporaryFile(prefix= 'fastqc_data_', delete= True)
+    shutil.copyfile('fastqc_data.txt', tmpf.name)
+    fastqcList= fastqc_md5.add_md5_fastqc(tmpf.name, 'd41d8cd98f00b204e9800998ecf8427e')
+    assert 'md5sum\td41d8cd98f00b204e9800998ecf8427e\n' in fastqcList
+
+def test_run():
+    cmd= "./fastqc_md5.py -i db001.test.fq.gz --fastqc ' --noextract ' "
+    p= subprocess.Popen(cmd, shell= True, stdout= subprocess.PIPE, stderr= subprocess.PIPE)
+    stderr, stdout= p.communicate()
+    done= True
+    try:
+        os.remove('db001.test.fq_fastqc.zip')
+    except OSError:
+        done= False
+    assert done
+    
+    
+    
