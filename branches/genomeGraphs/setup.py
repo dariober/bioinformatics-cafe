6f9b0a97b4f2e661da4f6eadf78c7fdb4a9d146e
dario.beraldi@gmail.com <dario.beraldi@gmail.com@7f1e2009-dfb0-8c77-38af-1b75acbe61a4>
2013-06-25 08:04:11 +0000

    genomeGraphs

--- branches/genomeGraphs/setup.py
+++ branches/genomeGraphs/setup.py
@@ -16,7 +16,7 @@ cd ../../
 
 setup(
    name = 'genomeGraphs',
-   version = '0.1.0',
+   version = '0.1.1a',
    author = 'Dario Beraldi',
    author_email = 'dario.beraldi<at>gmail<dot>com',
    url= '',
