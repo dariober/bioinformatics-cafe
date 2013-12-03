1d872493a8f24a0ecf7640e32ce401f11980059f
dario.beraldi@gmail.com <dario.beraldi@gmail.com@7f1e2009-dfb0-8c77-38af-1b75acbe61a4>
2013-12-03 09:35:46 +0000

    Add readMarkDuplicates.R

---
+++ trunk/readMarkDuplicates.R
@@ -0,0 +1,35 @@
+readMarkDuplicates<- function(x){
+    # Read output of picard MarkDuplicates
+    # x:
+    #   Metrics file from markDuplicates
+    # Return:
+    #   List with compenents metrics and hist. 
+    # --------------------------------------------------------------------------
+    mdup<- readLines(x)
+    
+    ## Read summary metrics:
+    headerIdx<- grep('## METRICS CLASS\tnet.sf.picard.sam.DuplicationMetrics', mdup) + 1
+    met<- mdup[headerIdx:(headerIdx + 1)]
+    header<- strsplit(met[1], '\t')[[1]]
+    metrics<- as.data.frame(t(strsplit(met[2], '\t')[[1]]), stringsAsFactors= FALSE)
+    colnames(metrics)<- header
+    for(i in 2:ncol(metrics)){
+        metrics[, i]<- as.numeric(metrics[, i])
+    }
+    
+    ## Histogram
+    header<- grep('BINx\tVALUE', mdup)
+    if(length(header) > 0){
+        histDup<- mdup[header:length(mdup)]
+        histDup<- t(sapply(strsplit(histDup, '\t'), function(x) x[1:2]))
+        colnames(histDup)<- histDup[1,]
+        histDup<- as.data.frame(histDup[2:nrow(histDup),], stringsAsFactors= FALSE, colClasses= c('numeric', 'numeric'))
+        histDup<- histDup[complete.cases(histDup),]
+    } else {
+        histDup<- NULL
+    }
+    ##
+    mdupList<- list(metrics= metrics, hist= histDup)
+    return(mdupList)
+}
+
