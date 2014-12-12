c0ce868ab72ebb9bc33ca1246559accebc9463c8
dario.beraldi@gmail.com <dario.beraldi@gmail.com@7f1e2009-dfb0-8c77-38af-1b75acbe61a4>
2014-12-12 14:36:05 +0000

    First commit readCountFromBAm

---
+++ trunk/readCountFromBam.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+docstring="Get read count from BAM file.
+\n\n
+This script parses the output of samtools idxstats to quickly get the total read
+count from a bam file.
+\n\n
+Requires awk and samtools on PATH.
+\n\n
+USAGE \n
+readCountFromBam.sh <aln.bam>
+\n\n
+OUTPUT \n
+# mapped (sum 3rd col) \n
+# mate unmapped (sum 4th col) \n
+# unmapped (* line) \n
+# ref size (sum 2nd col) \n
+"
+
+bam=$1
+
+#  || $bam = "-h" || $bam = "--help" 
+
+if [[ ${bam} = "" || ${bam} = "-h" || ${bam} == "--help" ]]
+then
+    echo -e $docstring
+    exit 1
+fi
+
+if [[ ! -f ${bam}.bai && ! -f ${bam%.bam}.bai ]]
+then
+    echo "Index file ${bam}.bai or ${bam%.bam}.bai not found"
+    exit 1
+fi
+
+s=$(type -P samtools)
+if [[ ${s} = "" ]]
+then
+    echo "samtools not found on PATH!"
+    exit 1
+fi
+
+samtools idxstats ${bam} \
+| awk 'BEGIN{OFS="\t"; FS="\t"} {
+    if($1 == "*"){
+        unmapped=$3+$4
+    } else {
+        mapped+=$3
+        mateun+=$4
+        refsize+=$2
+    }
+}END{print mapped, mateun, unmapped, refsize}'  
+
+exit 0
