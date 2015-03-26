8f6826ac5014723213932ef8c907d647d94b7b71
Dario Beraldi <dario.beraldi@gmail.com>
2015-03-26 11:45:16 +0000

    tableCat done

---
+++ tableCat/tableCat.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+import glob
+import os
+import gzip
+import re
+
+parser = argparse.ArgumentParser(description= """
+DESCRIPTION
+EXAMPLE
+REQUIRES:
+NOTES:
+""", formatter_class= argparse.RawTextHelpFormatter, prog= os.path.basename(__file__))
+
+parser.add_argument('--input', '-i',
+    required= True,
+    nargs= '+',
+    help='''Input files to concatenate
+''')
+
+parser.add_argument('--sep', '-s',
+    default= '\t',
+    help='''Separator used to right-bind the column with file id. Default to tab.
+''')
+
+parser.add_argument('--keepdir', '-kd',
+    action= 'store_true',
+    help='''Keep the the directory name in the output. Default is to strip it.
+''')
+
+parser.add_argument('--regex', '-r',
+    default= None,
+    help='''an optional regex to strip from the file name to make the ID nicer.
+''')
+
+parser.add_argument('--idColName', '-id',
+    default= 'file_id',
+    help='''Name for the additional column with the file id. Default 'file_id'.
+''')
+
+#parser.add_argument('--skip', '-S',
+#    type= int,
+#    default= 0,
+#    help='''Skip this many lines from *each* file.
+#''')
+
+parser.add_argument('--firsthdr', '-H',
+    action= 'store_true',
+    help='''First non-skipped line from *first* file is header and it is printed.
+    The first non-skipped line from following files is NOT printed.
+''')
+
+
+parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
+
+# -----------------------------------------------------------------------------
+
+def globToList(glist):
+    """Expand glob(s) to list of individual files. Duplicates removed!
+    """
+    if type(glist) != list:
+        sys.exit('Invalid arg')
+    globbed= []
+    for g in glist:
+        xglobbed= glob.glob(g)
+        for x in xglobbed:
+            globbed.append(x)
+    globbed= sorted(list(set(globbed)))
+    return globbed
+
+def xopen(x):
+    """Seemlessly open flat or gzip file.
+    x: File name
+    Return: Open file handle ready to iterate through
+    """
+    try:
+        fin= open(x)
+    except IOError:
+        try:
+            fin= gzip.open(x)
+        except IOError:
+            sys.exit('Cannot open %s' %(s))
+    return fin
+
+def parseID(filename, keepdir= False, regex= None):
+    """Parse the filename to optionally remove dir and a regex
+    """
+    id= filename
+    if not keepdir:
+        id= os.path.basename(filename)
+    if regex:
+        id= re.sub(regex, '', id)
+    return(id)
+
+if __name__ == '__main__':
+    args= parser.parse_args()
+    files= globToList(args.input)
+    if files == []:
+        sys.exit('No file found to concatenate!')
+    is_first_file= True
+    for f in files:
+        sys.stderr.write('File: %s ...' %(f))
+        fin= xopen(f)
+        id= parseID(f, args.keepdir, args.regex)
+        is_first_line= True
+        n= 0
+        for line in fin:
+            line2= line.rstrip('\n') + args.sep + id
+            if args.firsthdr:
+                if is_first_file and is_first_line:
+                    print line.rstrip('\n') + args.sep + args.idColName
+                    n+=1
+                else:
+                    if is_first_line:
+                        pass
+                    else:
+                        print line2
+                        n+=1
+                is_first_line= False
+            else:
+                print line2
+                n+=1
+        is_first_file= False
+        sys.stderr.write(' %s lines printed\n' %(n))
+    sys.exit()
+    
---
+++ tableCat/tableCatTest.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env py.test
+
+"""
+MEMO: Test functions MUST have "test_" as name prefix.
+"""
+
+import tableCat as tc
+import pytest
+import subprocess as sp
+
+def test_globToList_canGlobFilesFromList():
+    inglob= ['test/*.txt']
+    globbed= tc.globToList(inglob)
+    print globbed
+    assert ['test/a.txt', 'test/b.txt', 'test/c.txt'] == globbed
+
+    inglob= ['test/*.txt', 'test/*.txt', 'nonsense.txt']
+    globbed= tc.globToList(inglob)
+    assert ['test/a.txt', 'test/b.txt', 'test/c.txt'] == globbed
+
+def test_xopen_canOpenFlatOrGzFile():
+    fin= tc.xopen('test/a.txt')
+    fin= tc.xopen('test/a.txt.gz')
+    with pytest.raises(Exception):
+        tc.xopen('nonsense')
+    fin.close()
+
+def test_parseID_canParseFilename():
+    id= tc.parseID('test/a.txt', keepdir= False)
+    assert 'a.txt' == id
+    id= tc.parseID('test/a.txt', keepdir= True)
+    assert 'test/a.txt' == id
+    id= tc.parseID('test/a.txt', keepdir= False, regex= '\.txt$')
+    assert 'a' == id
+
+def test_runScript():
+    cmd= './tableCat.py -h'
+    p= sp.Popen(cmd, shell= True, stdout=sp.PIPE, stderr=sp.PIPE)
+    print(p.stderr.read())
+    p.communicate()
+    assert p.returncode == 0
+    
+    cmd= './tableCat.py -i test/a.txt test/b.txt'
+    p= sp.Popen(cmd, shell= True, stdout=sp.PIPE, stderr=sp.PIPE)
+    print(p.stderr.read())
+    p.communicate()
+    assert p.returncode == 0
+
+    cmd= './tableCat.py -i test/a.txt test/b.txt'
+    p= sp.Popen(cmd, shell= True, stdout=sp.PIPE, stderr=sp.PIPE)
+    out= p.stdout.read().split('\n')
+    p.communicate()
+    assert p.returncode == 0
+    assert 'hdr1\thdr2\thdr3\ta.txt' == out[0]
+    assert 'a\t1\t10\ta.txt' == out[1]
+    
