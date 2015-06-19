23a4902f66acb3af2b1cb0662c9b8060d9888368
Dario Beraldi <dario.beraldi@gmail.com>
2015-06-19 10:22:23 +0100

    Major-edits

--- htseq_matrix.py
+++ htseq_matrix.py
@@ -4,18 +4,20 @@ import os
 import sys
 import argparse
 import re
-import redmine
+
+VERSION= '0.1.0'
 
 parser = argparse.ArgumentParser(description= """
 DESCRIPTION
     Produces a matrix of genes (rows) and libraries (columns) from the output of
-    htseq-count. Output will be suitable for edgeR/DESeq.
-    Stdout will print the rows from "no_feature" to the end.
+    htseq-count. Output will be suitable for edgeR/DESeq after having removed 
+    unwanted rows like "__no_feature"
+    
 
 EXAMPLE
-    ## Produce matrix of all the files ending in .htseq, remove .htseq from file name
-    find . -name '*.htseq' | sort | htseq_matrix.py - -s '\.htseq$' htseq.matrix
-TODO:
+    # Matrix of all files ending in .htseq, remove suffix. 
+    # Exclude rows beginning with '__'. 
+    htseq_matrix.py *.htseq -s '\.htseq$' | grep -v '^__'
 
 """, formatter_class= argparse.RawTextHelpFormatter)
 
@@ -24,12 +26,6 @@ parser.add_argument('htseqfiles',
                    help='''One or more output files from htseq-count to be converted
 to matrix. Output files should have the same features in the same order. Use - to
 read the list of files from stdin.
-                   ''')
-
-parser.add_argument('--outmatrix', '-o',
-                   required= True,
-                   type= str,
-                   help='''Output file where the matrix will be written to.
                    ''')
 
 parser.add_argument('--strip', '-s',
@@ -47,16 +43,14 @@ parser.add_argument('--keepdir', '-d',
                    help='''Do not remove directory path from file names (by default
 it is removed).
                    ''')
-
-parser.add_argument('--redmine',
-                    action= 'store_true',
-                   help='''Format the report output as a table for redmine (default
-is a tab separatetd table).
-                   ''')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s %(VERSION)s' %{'prog': '%(prog)s', 'VERSION': VERSION})
 
 args = parser.parse_args()
 
 # ------------------------------------------------------------------------------
+
+NAME_COL= 0 # Index of feature name and count columns
+CNT_COL= 1
 
 if args.htseqfiles == ['-']:
     args.htseqfiles= sys.stdin.readlines()
@@ -70,61 +64,33 @@ if args.strip is not None:
     header= [re.sub(args.strip, '', x) for x in header]
 header= ['feature_id'] + header
 
-outf= open(args.outmatrix, 'w')
-outf.write('\t'.join(header) + '\n')
-if args.redmine:
-    print(redmine.list2table(header, header= True))
-else:
-    header= '\t'.join(header)
-    print(header)
+print('\t'.join(header))
     
-is_feature= True ## Mark to say whether the current line is feature to be inlcuded in the matrix
-                 ## Will turn to False once the row 'no_feature' is found
-
 infiles= []
 for lib in args.htseqfiles:
     infiles.append(open(lib))
-read_count= [0] * len(infiles) ## Keep track of how many reads are mapped to feature
 
 while True:
     for i in range(0, len(infiles)):
         if i == 0:
-            fline= infiles[i].readline()
-            fline= fline.strip().split('\t')
-            feature_id= fline[0]
-            if feature_id == 'no_feature':
-                is_feature= False
+            # First file: include gene name and count
+            fline= infiles[i].readline().strip().split('\t')
+            feature_id= fline[NAME_COL]
             if fline == ['']:
                 break
-            line= fline
-            if is_feature:
-                read_count[i] += int(line[1])
+            line= [fline[NAME_COL], fline[CNT_COL]]
         else:
-            fline= infiles[i].readline()
-            fline= fline.strip().split('\t')
+            # Following files: Include only count
+            fline= infiles[i].readline().strip().split('\t')
             if fline == ['']:
                 break
-            next_feature= fline[0]
-            if next_feature != feature_id:
-                sys.exit('Input files do not seem to have the same features and/or they are not in the same order: %s, %s' %(feature_id, next_feature))
-            line.append(fline[1])
-            if is_feature:
-                read_count[i] += int(fline[1])
+            this_feature= fline[NAME_COL]
+            if this_feature != feature_id:
+                sys.exit('Input files do not seem to have the same features and/or they are not in the same order: %s, %s' %(feature_id, this_feature))
+            line.append(fline[CNT_COL])
     if fline == ['']:
         break
-    if is_feature:
-        outf.write('\t'.join(line) + '\n')
-    else:
-        if args.redmine:
-            print(redmine.list2table(line))
-        else:
-            print('\t'.join(line))
-read_count= [str(x) for x in read_count]
-if args.redmine:
-    print(redmine.list2table(['assigned'] + read_count))
-else:
-    print('\t'.join(['assigned'] + read_count))
+    print('\t'.join(line))
 for f in infiles:
     f.close()    
-outf.close()
 sys.exit()
