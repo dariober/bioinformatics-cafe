655c19177ea438ab58c4eb014010085ffb218a60
Dario Beraldi <dario.beraldi@gmail.com>
2015-06-23 11:26:47 +0100

    Can read gzip file, optional column indexes for name & count. Fix bug eof.

--- htseq_matrix.py
+++ htseq_matrix.py
@@ -1,9 +1,10 @@
-#!/home/berald01/.local/bin/python
+#!/usr/bin/env python
 
 import os
 import sys
 import argparse
 import re
+import gzip
 
 VERSION= '0.1.0'
 
@@ -25,7 +26,7 @@ parser.add_argument('htseqfiles',
                    nargs= '+',
                    help='''One or more output files from htseq-count to be converted
 to matrix. Output files should have the same features in the same order. Use - to
-read the list of files from stdin.
+read the list of files from stdin. File ending with .gz are opened with gzip.
                    ''')
 
 parser.add_argument('--strip', '-s',
@@ -37,20 +38,24 @@ The replacement is applied to the file name only unless --keepdir is active. Wit
 --keepdir the replacement is applied to the whole path as passed to the list of files.
                    ''')
 
+parser.add_argument('--nameCol', '-n', type= int, default= 1, help= 'Column index of the gene or feature name. 1-based, default 1 (1st column)')
+parser.add_argument('--cntCol', '-c', type= int, default= 2, help= 'Column index of the counts. 1-based, default 2 (2nd column)')
+
 parser.add_argument('--keepdir', '-d',
                    required= False,
                    action= 'store_true',
                    help='''Do not remove directory path from file names (by default
 it is removed).
                    ''')
+
 parser.add_argument('--version', '-v', action='version', version='%(prog)s %(VERSION)s' %{'prog': '%(prog)s', 'VERSION': VERSION})
 
 args = parser.parse_args()
 
 # ------------------------------------------------------------------------------
 
-NAME_COL= 0 # Index of feature name and count columns
-CNT_COL= 1
+name_col= args.nameCol - 1
+cnt_col= args.cntCol - 1
 
 if args.htseqfiles == ['-']:
     args.htseqfiles= sys.stdin.readlines()
@@ -68,26 +73,29 @@ print('\t'.join(header))
     
 infiles= []
 for lib in args.htseqfiles:
-    infiles.append(open(lib))
+    if(lib.endswith('.gz')):
+	infiles.append(gzip.open(lib, 'rb'))
+    else:
+        infiles.append(open(lib))
 
 while True:
     for i in range(0, len(infiles)):
         if i == 0:
             # First file: include gene name and count
             fline= infiles[i].readline().strip().split('\t')
-            feature_id= fline[NAME_COL]
             if fline == ['']:
                 break
-            line= [fline[NAME_COL], fline[CNT_COL]]
+            feature_id= fline[name_col]        
+            line= [fline[name_col], fline[cnt_col]]
         else:
             # Following files: Include only count
             fline= infiles[i].readline().strip().split('\t')
             if fline == ['']:
                 break
-            this_feature= fline[NAME_COL]
+            this_feature= fline[name_col]
             if this_feature != feature_id:
                 sys.exit('Input files do not seem to have the same features and/or they are not in the same order: %s, %s' %(feature_id, this_feature))
-            line.append(fline[CNT_COL])
+            line.append(fline[cnt_col])
     if fline == ['']:
         break
     print('\t'.join(line))
