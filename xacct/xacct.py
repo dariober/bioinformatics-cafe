dc004fd5db1cc7e3d21e788a69bb68b88822b51b
dariober <dario.beraldi@gmail.com>
2017-10-09 08:32:46 +0100

    Switch to argparse

--- xacct/xacct.py
+++ xacct/xacct.py
@@ -1,42 +1,38 @@
 #!/usr/bin/env python3
 
-import sys
 import subprocess
 import csv
 import collections
 from io import StringIO
+import argparse
+import os
 
-docstring= """
-sacct_easy.py [-tsv] [sacct-args]
-
+parser = argparse.ArgumentParser(description= """
 Execute slurm sacct and return a friendly tabular output. 
 Values in MaxRSS and ReqMem are in MB units.
 
-OPTIONS
-
--tsv       
-    Print tab separated columns (better for further processing)
-
-sacct-args 
-    Further args to sacct, e.g. `-S 2017-09-10`. Do not include `--format`
-
 EXAMPLES
 
-xacct.py               
-xacct.py -S 2016-12-01  # Show jobs starting from YYYY-MM-DD
+xacct.py
+xacct.py -- -S 2016-12-01  # Show jobs starting from YYYY-MM-DD
 
 # Sort by memory usage
 xacct.py -tsv | tail -n+2 | sort -t'   ' -k4,4n
-"""
+""", formatter_class= argparse.RawTextHelpFormatter, prog= os.path.basename(__file__))
 
-if '-h' in sys.argv or '--help' in sys.argv:
-    print(docstring)
-    sys.exit()
+parser.add_argument('--tsv', '-tsv',
+                   action= "store_true",
+                   help='''Print columns separated by TAB (better for further processing) 
+instead of tabulating them using spaces (better for eyeballing)''')
 
-asTsv= False
-if '-tsv' in sys.argv:
-    asTsv= True
-    sys.argv.remove('-tsv')
+parser.add_argument('sacct_args',
+                   nargs= "*",
+                   help='''Further args to sacct, e.g. `-S 2017-09-10`. Do not include `--format`.
+Use `--` to add slurm args.''')
+
+parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
+
+args= parser.parse_args()
 
 def normalizeMem(x):
     """We use binary multiplier instead of decimal to convert kilo and giga to
@@ -78,12 +74,12 @@ def tabulate(line, maxlen, asTsv):
     row_fmt= "{:<8}{:<%s}{:<16}{:<8}{:<8}{:<10}{:<12}{:<30}" % (maxlen + 2)
     return row_fmt.format(*line)
 
-sacct= subprocess.check_output(['sacct', '--parsable2', '--format=JobID,JobName%50,NodeList,MaxRSS,ReqMem,AllocCPUS,Elapsed,State'] + sys.argv[1:])
+sacct= subprocess.check_output(['sacct', '--parsable2', '--format=JobID,JobName%50,NodeList,MaxRSS,ReqMem,AllocCPUS,Elapsed,State'] + args.sacct_args)
 
 reader = csv.DictReader(sacct.decode().split('\n'), delimiter='|')
 
 maxlen= maxNameLen(sacct)
-print(tabulate(reader.fieldnames, maxlen, asTsv))
+print(tabulate(reader.fieldnames, maxlen, args.tsv))
 
 try:
     idjob= None
@@ -95,13 +91,13 @@ try:
                     # This line is not a batch job, so the previous line
                     # can be printed as it doesn't have an associated batch job.
                     lst= list(idjob.values())
-                    print(tabulate(lst, maxlen, asTsv))
+                    print(tabulate(lst, maxlen, args.tsv))
                 idjob= line
         else:
             # This is a batch job. So associate to it the job ID line    
             if idjob['JobID'] == line['JobID'].replace('.batch', ''):
                 lst= list(fillInJob(idjob, line).values())
-                print(tabulate(lst, maxlen, asTsv))
+                print(tabulate(lst, maxlen, args.tsv))
             else:
                 print(idjob)
                 print(line)
@@ -110,8 +106,8 @@ try:
 
     if idjob is not None:
         lst= list(idjob.values())
-        print(tabulate(lst, maxlen, asTsv))
-    print(tabulate(reader.fieldnames, maxlen, asTsv))
+        print(tabulate(lst, maxlen, args.tsv))
+    print(tabulate(reader.fieldnames, maxlen, args.tsv))
 
 except (BrokenPipeError, IOError):
     # This is to avoid stack trace in e.g. `sacct_easy.py | head`
